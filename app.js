b193a1e712cec5b0bc62e801f432e6212d448c2b
Zane Campbell <zanegitinit@gmail.com>
2019-03-02 09:02:47 -0800

    setup yargv groundwork

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.blue.inverse.bold('Success!'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!');
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+});
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function () {
+        console.log('Listing all notes');
+    }
+});
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading note',
+    handler: function () {
+        console.log('Reading the note');
+    }
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
